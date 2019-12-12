f21493da520d5d8ae5e3576151d8c476037d480e
yorkdick <yorkdick@163.com>
2019-12-12 21:37:34 +0800

    update

--- java-test/src/main/java/com/myself/java/test/proxyTest/DynamicProxyTest.java
+++ java-test/src/main/java/com/myself/java/test/proxyTest/DynamicProxyTest.java
@@ -1,23 +1,34 @@
 package com.myself.java.test.proxyTest;
 
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 
 public class DynamicProxyTest {
     public static void main(String[] args) {
-        ProxyInterface proxyInterface = new NoramlClass();
-        ProxyInterface proxy = getProxyInstance(proxyInterface);
+        NoramlClass noramlClass = new NoramlClass();
+        ProxyInterface proxy = getProxyInstance(noramlClass);
+
+        noramlClass.function1();
 
         proxy.function1();
         proxy.function2("4444");
     }
 
     public static <T> T getProxyInstance(T obj){
-        return (T)Proxy.newProxyInstance(obj.getClass().getClassLoader(),
-                obj.getClass().getInterfaces(),(proxy,method,args)->{
-                    System.out.println("Dynamic before invoke method.");
-                    Object returnValue = method.invoke(obj, args);
-                    System.out.println("Dynamic after invoke method.");
-                    return returnValue;
-                });
+        ClassLoader classLoader = obj.getClass().getClassLoader();
+        Class<?>[] interfaces = obj.getClass().getInterfaces();
+        InvocationHandler invocationHandler = new InvocationHandler(){
+            @Override
+            public Object invoke(Object proxy, Method targetMethod, Object[] args) throws Throwable {
+                System.out.println("Dynamic before invoke method."+targetMethod);
+
+                Object returnValue = targetMethod.invoke(obj, args);
+
+                System.out.println("Dynamic after invoke method."+targetMethod);
+                return returnValue;
+            }
+        };
+        return (T)Proxy.newProxyInstance(classLoader,interfaces,invocationHandler);
     }
 }
